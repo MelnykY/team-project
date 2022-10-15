9e332dde0c15b040cb65b7f740896498e23e8f94
MelnykY <106350059+MelnykY@users.noreply.github.com>
2022-10-15 20:45:47 +0000

    Deploying to gh-pages from @ MelnykY/team-project@b7afbe353375b59b3787b71a0ad474e44d38bac5 🚀

---
+++ index.71950c15.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-locations-open]"),closeModalBtn:document.querySelector("[data-locations-close]"),modal:document.querySelector("[data-locations]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.71950c15.js.map
