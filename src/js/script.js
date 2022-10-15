4d3fcf64b668cb940bcfeb338e64b47758c5a639
Yaroslav12002 <my.mail12002@gmail.com>
2022-10-15 13:21:04 +0300

    added js for slick slider

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:true, 
+    dots:true,
+    adaptiveHeight:false,
+    speed:1000,
+    easing:'ease',
+    autoplay:true,
+    autoplaySpeed:5000,
+    waitForAnimate:false
+});
+});
