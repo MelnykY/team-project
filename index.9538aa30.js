5ab530a884177c0363faa259f36d921dbf0796bc
MelnykY <106350059+MelnykY@users.noreply.github.com>
2022-10-16 20:35:18 +0000

    Deploying to gh-pages from @ MelnykY/team-project@94d7caa1caf878f29c15fdf608e16271a09c5bec 🚀

---
+++ index.9538aa30.js
@@ -0,0 +1,2 @@
+$((function(){$(".reviews-slider").slick({arrows:!0,dots:!0,adaptiveHeight:!1,speed:1e3,easing:"ease",autoplay:!0,autoplaySpeed:3e4,waitForAnimate:!1})})),$((function(){$(".gallery__slider").slick({fade:!0,arrows:!1,autoplay:!0,autoplaySpeed:3e3,speed:1e3})}));
+//# sourceMappingURL=index.9538aa30.js.map
