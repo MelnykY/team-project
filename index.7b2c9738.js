42e5f3e46c55a98ca11f1efe29f71679a80b7ede
MelnykY <106350059+MelnykY@users.noreply.github.com>
2022-10-18 19:10:05 +0000

    Deploying to gh-pages from @ MelnykY/team-project@1ffd608f6a4385fb3c17926760546721133d917e 🚀

---
+++ index.7b2c9738.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-locations-open]"),closeModalBtn:document.querySelector("[data-locations-close]"),modal:document.querySelector("[data-locations]")};function e(){document.body.classList.toggle("modal-open"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.7b2c9738.js.map
--- index.8f247240.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-locations-open]"),closeModalBtn:document.querySelector("[data-locations-close]"),modal:document.querySelector("[data-locations]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.8f247240.js.map
